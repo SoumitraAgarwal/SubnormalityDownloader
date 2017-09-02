b9dc6cb449f33d30fcb5b8bfb4dfc21b570b5a57
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-09-03 04:09:14 +0530

    Get other works

---
+++ others.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import urllib2
+import requests
+import pandas as pd
+import shutil
+import time
+import os
+
+millis = int(round(time.time() * 1000))
+
+
+proxies = {
+  'http': 'http://172.16.114.112:3128',
+  'https': 'https://172.16.114.112:3128',
+}
+
+urls = []
+for i in range(29,369):
+
+	url = "http://www.viruscomix.com/page" + str(i) + ".html"
+	print("Getting page for " + str(i))
+	while(True):
+		try:
+			page = requests.get(url,proxies=proxies)
+		except requests.exceptions.RequestException as e:  # This is the correct syntax
+			print(e)
+			time.sleep(5)
+			continue
+		break
+
+	html = page.content
+	soup = BeautifulSoup(html,'lxml')
+	
+	Nat = soup.find('img')
+
+	if(Nat is not None):
+		image_source = "http://www.viruscomix.com/" + Nat['src']
+		
+		millis = int(round(time.time() * 1000))-millis	
+		print("Book after " + str(millis) + "ms!\nTotal distinct books = " + str(len(os.listdir('Others/'))))	
+		millis = int(round(time.time() * 1000))
+		
+		while(True):
+			try:
+				response = requests.get(image_source, stream=True,proxies=proxies)
+			except requests.exceptions.RequestException as e:  # This is the correct syntax
+				print(e)
+				time.sleep(5)
+				continue
+			break
+		
+		with open('Others/'+ Nat['src'], 'wb') as out_file:
+			shutil.copyfileobj(response.raw, out_file)
+		del response
